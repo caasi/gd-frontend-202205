2f66c2ff91cd0ca66fdd4631fb446720cad5a19f
joy21fs <1017peilin.yu@gmai.com>
2022-05-05 13:49:29 +0800

    change link style at hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import { useState, useEffect, useRef } from 'react'
-import { User } from './libs/user'
-import { Post } from './libs/post'
-import { getUserList, getPostListById } from './libs/api'
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import { User } from "./libs/user";
+import { Post } from "./libs/post";
+import { getUserList, getPostListById } from "./libs/api";
+import "./App.css";
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const [filter, setFilter] = useState('')
-  const [currentUser, setCurrentUser] = useState<string>()
-  const [users, setUsers] = useState<User[]>([])
-  const [posts, setPosts] = useState<Post[]>([])
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [filter, setFilter] = useState("");
+  const [currentUser, setCurrentUser] = useState<string>();
+  const [users, setUsers] = useState<User[]>([]);
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-    getUserList()
-      .then((users) => setUsers(users))
-  })
+    getUserList().then((users) => setUsers(users));
+  });
 
   useEffect(() => {
     if (currentUser) {
-      getPostListById(currentUser)
-        .then((posts) => setPosts(posts))
+      getPostListById(currentUser).then((posts) => setPosts(posts));
     }
-  }, [currentUser])
+  }, [currentUser]);
 
   return (
     <div className="App">
@@ -34,45 +32,45 @@ function App() {
             type="text"
             placeholder="filter"
             onChange={() => {
-              if (inputRef.current) setFilter(inputRef.current.value)
+              if (inputRef.current) setFilter(inputRef.current.value);
             }}
           />
           <ul>
             {users
-              .filter((user) => new RegExp(filter, 'i').test(user.name))
+              .filter((user) => new RegExp(filter, "i").test(user.name))
               .map((user) => (
                 <li key={user.type}>
                   <a
+                    className="link--username"
                     href="#"
                     onClick={(evt) => {
-                      evt.preventDefault()
-                      setCurrentUser(user._id)
-                    }}>
+                      evt.preventDefault();
+                      setCurrentUser(user._id);
+                    }}
+                  >
                     {user.name}
                   </a>
                   &nbsp;
                   {user.description}
                 </li>
-              ))
-            }
+              ))}
           </ul>
         </section>
         <section>
           <h2>Posts</h2>
-          {posts.length === 0
-            ? <p>No posts</p>
-            : <ul>
+          {posts.length === 0 ? (
+            <p>No posts</p>
+          ) : (
+            <ul>
               {posts.map((post) => (
-                <li key={post._id}>
-                  {post.content}
-                </li>
+                <li key={post._id}>{post.content}</li>
               ))}
             </ul>
-          }
+          )}
         </section>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
