7d493b9111dcc3fb977f8aeaf5142d6556e213a2
kk <a631953720@gmail.com>
2022-05-06 13:39:54 +0800

    refector: modify App code structor

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,72 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import { User } from './libs/user'
 import { Post } from './libs/post'
 import { getUserList, getPostListById } from './libs/api'
 import './App.css'
 
-function App() {
+const SearchTextField = (props: { setFilter: (filter: string) => void }) => {
+  const { setFilter } = props;
   const inputRef = useRef<HTMLInputElement>(null)
+  return (
+    <input
+      ref={inputRef}
+      type="text"
+      placeholder="filter"
+      onChange={() => {
+        if (inputRef.current) setFilter(inputRef.current.value)
+      }}
+    />
+  );
+};
+
+function App() {
   const [filter, setFilter] = useState('')
   const [currentUser, setCurrentUser] = useState<string>()
   const [users, setUsers] = useState<User[]>([])
   const [posts, setPosts] = useState<Post[]>([])
 
+  const UserList = useCallback(() => {
+    const List = users
+    .filter((user) => new RegExp(filter, 'i').test(user.name))
+    .map((user, idx) => (
+      <li key={`${user.type}-${idx}`}>
+        <a
+          href="#"
+          onClick={(evt) => {
+            evt.preventDefault()
+            setCurrentUser(user._id)
+          }}>
+          {user.name}
+        </a>
+        &nbsp;
+        {user.description}
+      </li>
+    ))
+
+    return (
+      <ul>
+        {List}
+      </ul>
+    );
+  }, [users, filter]);
+
+  const PostList = useCallback(() => {
+    if (posts.length === 0) return <p>No posts</p>;
+    return (
+      <ul>
+        {posts.map((post) => (
+          <li key={post._id}>
+            {post.content}
+          </li>
+        ))}
+      </ul>
+    );
+  }, [posts]);
+
   useEffect(() => {
     getUserList()
       .then((users) => setUsers(users))
   })
-
   useEffect(() => {
     if (currentUser) {
       getPostListById(currentUser)
@@ -29,46 +80,12 @@ function App() {
         <h1>Hello Vite + React!</h1>
         <section>
           <h2>Users</h2>
-          <input
-            ref={inputRef}
-            type="text"
-            placeholder="filter"
-            onChange={() => {
-              if (inputRef.current) setFilter(inputRef.current.value)
-            }}
-          />
-          <ul>
-            {users
-              .filter((user) => new RegExp(filter, 'i').test(user.name))
-              .map((user, idx) => (
-                <li key={`${user.type}-${idx}`}>
-                  <a
-                    href="#"
-                    onClick={(evt) => {
-                      evt.preventDefault()
-                      setCurrentUser(user._id)
-                    }}>
-                    {user.name}
-                  </a>
-                  &nbsp;
-                  {user.description}
-                </li>
-              ))
-            }
-          </ul>
+          <SearchTextField setFilter={setFilter} />
+          {UserList()}
         </section>
         <section>
           <h2>Posts</h2>
-          {posts.length === 0
-            ? <p>No posts</p>
-            : <ul>
-              {posts.map((post) => (
-                <li key={post._id}>
-                  {post.content}
-                </li>
-              ))}
-            </ul>
-          }
+          {PostList()}
         </section>
       </div>
     </div>
